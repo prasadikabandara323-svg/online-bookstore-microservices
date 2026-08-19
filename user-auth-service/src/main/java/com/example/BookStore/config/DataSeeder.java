29160d8295a84d3e953dbaf15562fe2b6cae5d8e
ayodhyaranathunge <ayodhyaranathunge@gmail.com>
2026-08-20 02:29:38 +0530

    Restructure client-app with per-service Dockerfiles, unify auth folder, add DataSeeder

---
+++ user-auth-service/src/main/java/com/example/BookStore/config/DataSeeder.java
@@ -0,0 +1,43 @@
+package com.example.BookStore.config;
+
+import java.time.LocalDateTime; // Role එක import කරගැනීම
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.BookStore.entity.Role;
+import com.example.BookStore.entity.User;
+import com.example.BookStore.repository.UserRepository;
+
+@Configuration
+public class DataSeeder {
+
+    @Bean
+    public CommandLineRunner initDatabase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (userRepository.count() == 0) {
+                User admin = new User();
+                admin.setFullName("prasadika");
+                admin.setEmail("prasadikabandara323@gmail.com");
+                admin.setPassword(passwordEncoder.encode("123456"));
+                admin.setRole(Role.ADMIN); // String වෙනුවට Role Enum එක භාවිතා කිරීම
+                admin.setEnabled(true);
+                admin.setCreatedAt(LocalDateTime.now());
+
+                User user = new User();
+                user.setFullName("Ayodhya");
+                user.setEmail("ayodhyaranathunge@gmail.com");
+                user.setPassword(passwordEncoder.encode("123456"));
+                user.setRole(Role.USER); // String වෙනුවට Role Enum එක භාවිතා කිරීම
+                user.setEnabled(true);
+                user.setCreatedAt(LocalDateTime.now());
+
+                userRepository.saveAll(List.of(admin, user));
+                System.out.println("✅ User Auth Service Data Seeded Successfully!");
+            }
+        };
+    }
+}
